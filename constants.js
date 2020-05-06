cf0b8fe3158dfb08fa70b562a8a9c28403f99634
Daniel Zhang <danielzhang@Daniels-MacBook-Pro.local>
2020-05-06 15:47:47 -0700

    constants.js added

---
+++ constants.js
@@ -0,0 +1,14 @@
+let IP = 'localhost';
+let PORT = '50541';
+let keyBind = {
+  w: "Move: up",
+  s: "Move: down",
+  d: "Move: right",
+  a: "Move: left",
+  e: "Say: hey"
+}
+module.exports = {
+  IP,
+  PORT,
+  keyBind
+};
