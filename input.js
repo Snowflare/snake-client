9209c0d5737228cfee994a35f3332aa87527f571
Daniel Zhang <danielzhang@Daniels-MacBook-Pro.local>
2020-05-06 15:31:20 -0700

    play.js and input.js improved

--- input.js
+++ input.js
@@ -2,12 +2,28 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+let connection; 
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+  
   stdin.on('data', (key) => {
     handleUserInput(key);
+    if (key === 'w'){
+      conn.write("Move: up")
+    }
+    if (key === 's'){
+      conn.write("Move: down")
+    }
+    if (key === 'a'){
+      conn.write("Move: left")
+    }
+    if (key === 'd'){
+      conn.write("Move: right")
+    }
   });
   stdin.resume();
   return stdin;
